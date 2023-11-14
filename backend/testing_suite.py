0af3312eee2900dd1fd5f75e5be0884dd2c9f06f
ANQ-Nguyen <93494123+ANQ-Nguyen@users.noreply.github.com>
2023-11-13 19:08:41 -0500

    Set up the testing suite better and finished all tests relating to making calls to Superset API

--- backend/testing_suite.py
+++ backend/testing_suite.py
@@ -1,6 +1,7 @@
 '''
 To run this test file, run the command: pytest testing_suite.py
 '''
+import unittest
 
 from backend.routes.dashboard_handler import DashboardHandler
 from backend.routes.changes_handler import ChangesHandler
@@ -12,7 +13,6 @@ from flask import app as flask_app
 import pytest
 
 # Initialize instances of the handlers
-
 # dashboard_handler = DashboardHandler()
 # changes_handler = ChangesHandler()
 
@@ -40,7 +40,11 @@ def teardown():
 # Basic testing suite for each CRUD operation
 
 
-class TestSuite:
+class TestSuite(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.access_token = get_access_token()
 
     # Version Control Tests
     def test_add_dashboard(self, client):
@@ -49,16 +53,16 @@ class TestSuite:
     def test_propagate_changes(self, client):
         pass
 
-    def test_get_dashboard(self, client):
-        '''
-        Testing to see if we can access thd ashboard test_4 froom our database successfully       
-        '''
-        
-        response = client.get("deliverable-2-12-3-mistr156-mongarud.vercel.app/api/get-dashboard/41012f4e-7b94-4945-a039-02b65c7af1f9")
-
-        curr_db = dashboard_handler.get_dashboard("41012f4e-7b94-4945-a039-02b65c7af1f9")
-
-        assert curr_db['dashboard_name'] == "test_4"
+    # def test_get_dashboard(self, client):
+    #     '''
+    #     Testing to see if we can access thd ashboard test_4 froom our database successfully
+    #     '''
+    #
+    #     response = client.get("deliverable-2-12-3-mistr156-mongarud.vercel.app/api/get-dashboard/41012f4e-7b94-4945-a039-02b65c7af1f9")
+    #
+    #     curr_db = dashboard_handler.get_dashboard("41012f4e-7b94-4945-a039-02b65c7af1f9")
+    #
+    #     assert curr_db['dashboard_name'] == "test_4"
 
     def test_delete_dashboard(self, client):
         pass
@@ -76,9 +80,8 @@ class TestSuite:
         assert response.status_code == 200
         assert 'access_token' in response.json()
 
-
     def test_valid_get_csrf_token(self):
-        access_token = get_access_token()
+        access_token = self.access_token
         headers = {"Authorization": "Bearer " + access_token}
 
         response = requests.get(SUPERSET_INSTANCE_URL + CSRF_TOKEN_ENDPOINT, headers=headers)
@@ -86,14 +89,45 @@ class TestSuite:
         assert response.status_code == 200
         assert 'result' in response.json()
 
-    def test_valid_get_dashboards(self):
-        pass
+    def test_valid_response_get_dashboards(self):
+        access_token = self.access_token
+        headers = {"Authorization": "Bearer " + access_token}
+
+        response = requests.get(SUPERSET_INSTANCE_URL + DASHBOARD_ENDPOINT, headers=headers)
+
+        assert response.status_code == 200
+        assert 'result' in response.json()
+        if response.json()['result']:
+            assert 'id' in response.json()['result'][0]
+            assert 'dashboard_title' in response.json()['result'][0]
 
     def test_valid_get_charts(self):
-        pass
+        access_token = self.access_token
+        headers = {"Authorization": "Bearer " + access_token}
+        dashboard = get_dashboards(access_token)[0]
+
+        chart_endpoint = f'{SUPERSET_INSTANCE_URL}{DASHBOARD_ENDPOINT}{str(dashboard[0])}/charts'
+
+        response = requests.get(chart_endpoint, headers=headers)
+
+        assert response.status_code == 200
+        assert 'result' in response.json()
+        if response.json()['result']:
+            assert 'id' in response.json()['result'][0]
+            assert 'slice_name' in response.json()['result'][0]
 
     def test_valid_get_datasets(self):
-        pass
+        access_token = self.access_token
+        headers = {"Authorization": "Bearer " + access_token}
+
+        response = requests.get(SUPERSET_INSTANCE_URL + DATASET_ENDPOINT, headers=headers)
+
+        assert response.status_code == 200
+        assert 'result' in response.json()
+        if response.json()['result']:
+            assert 'table_name' in response.json()['result'][0]
+            assert 'database' in response.json()['result'][0]
+            assert 'database_name' in response.json()['result'][0]['database']
 
     # Backend API Call Tests
     def test_valid_get_all_dashboards(self):
